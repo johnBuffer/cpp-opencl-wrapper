366ebf6109dc958d32e887ba332b5f90d1aa2ea9
Jean Tampon <jean.tampon@gmail.com>
2020-02-28 20:39:10 +0100

    Adding features support

--- include/ocl_wrapper.hpp
+++ include/ocl_wrapper.hpp
@@ -2,18 +2,199 @@
 
 #include <CL/opencl.h>
 #include <vector>
+#include <fstream>
+#include <sstream>
 
 
 namespace oclw
 {
+	enum PlatformType {
+		CPU = CL_DEVICE_TYPE_CPU,
+		GPU = CL_DEVICE_TYPE_GPU
+	};
+
+	enum MemoryObjectReadMode {
+		ReadOnly = CL_MEM_READ_ONLY,
+		ReadWrite = CL_MEM_READ_WRITE
+	};
+
+	class MemoryObject
+	{
+	public:
+		template<typename T>
+		MemoryObject(cl_context context, std::vector<T>& data, int32_t mode)
+		{
+			const uint64_t element_count = data.size();
+			m_memory_object = clCreateBuffer(context, mode, sizeof(T) * element_count, data.data(), NULL);
+		}
+
+		MemoryObject(cl_context context, uint32_t element_size, uint64_t element_count, int32_t mode)
+		{
+			m_memory_object = clCreateBuffer(context, mode, element_size * element_count, NULL, NULL);
+		}
+
+		operator bool() const
+		{
+			return m_memory_object;
+		}
+
+	private:
+		cl_mem m_memory_object;
+	};
+
+
+	class Kernel
+	{
+
+	};
+
+
+	class Program
+	{
+	public:
+		Program(cl_program program = nullptr)
+			: m_program(program)
+		{
+
+		}
+
+		Program(cl_context context, const std::string& source, cl_device_id device)
+			: m_program(nullptr)
+		{
+			int32_t err_num;
+			cl_program program = nullptr;
+			const char *src_str = source.c_str();
+			program = clCreateProgramWithSource(context, 1, (const char**)&src_str, NULL, NULL);
+			if (program == NULL) {
+				return;
+			}
+
+			err_num = clBuildProgram(program, 0, NULL, NULL, NULL, NULL);
+			if (err_num != CL_SUCCESS)
+			{
+				// Determine the reason for the error
+				char buildLog[16384];
+				clGetProgramBuildInfo(program, device, CL_PROGRAM_BUILD_LOG,
+					sizeof(buildLog), buildLog, NULL);
+				std::cerr << "Error in kernel: " << std::endl;
+				std::cerr << buildLog << std::endl;
+				clReleaseProgram(program);
+				return;
+			}
+
+			m_program = program;
+		}
+
+		operator bool() const
+		{
+			return m_program;
+		}
+
+		cl_kernel createKernel(const std::string& kernel_name)
+		{
+			return clCreateKernel(m_program, kernel_name.c_str(), NULL);
+		}
+
+	private:
+		cl_program m_program;
+	};
+
+
+	class Context
+	{
+	public:
+		Context(cl_context raw_context = nullptr)
+			: m_context(raw_context)
+		{}
+
+		operator bool() const
+		{
+			return m_context;
+		}
+
+		Context(cl_platform_id platform_id, PlatformType type)
+		{
+			cl_int errNum;
+			cl_context context = nullptr;
+			cl_context_properties contextProperties[] = {
+				CL_CONTEXT_PLATFORM,
+				(cl_context_properties)platform_id,
+				0
+			};
+
+			context = clCreateContextFromType(contextProperties, type, NULL, NULL, &errNum);
+			if (errNum == CL_SUCCESS) {
+				m_context = context;
+			}
+		}
+
+		const std::vector<cl_device_id>& getDevices()
+		{
+			m_devices.clear();
+			cl_int err_num;
+			std::size_t device_buffer_size = 0;
+			err_num = clGetContextInfo(m_context, CL_CONTEXT_DEVICES, 0, NULL, &device_buffer_size);
+			// Check everything is OK
+			if (err_num != CL_SUCCESS) {
+				return m_devices;
+			}
+			if (device_buffer_size <= 0) {
+				return m_devices;
+			}
+			const uint64_t devices_count = device_buffer_size / sizeof(cl_device_id);
+			m_devices.resize(devices_count);
+			err_num = clGetContextInfo(m_context, CL_CONTEXT_DEVICES, device_buffer_size, m_devices.data(), NULL);
+			if (err_num != CL_SUCCESS) {
+				return m_devices;
+			}
+
+			return m_devices;
+		}
+
+		cl_command_queue createQueue(cl_device_id device)
+		{
+			cl_command_queue command_queue = nullptr;			
+			command_queue = clCreateCommandQueue(m_context, device, 0, NULL);
+			if (!command_queue) {
+				return nullptr;
+			}
+			return command_queue;
+		}
+
+		Program createProgram(cl_device_id device, const char* fileName) const
+		{
+			std::ifstream kernel_file(fileName, std::ios::in);
+			if (!kernel_file.is_open())
+			{
+				return nullptr;
+			}
+			std::ostringstream oss;
+			oss << kernel_file.rdbuf();
+
+			return Program(m_context, oss.str(), device);
+		}
+
+		template<typename T>
+		MemoryObject createMemoryObject(std::vector<T>& data, int32_t mode = ReadOnly)
+		{
+			return MemoryObject(m_context, data, mode);
+		}
+
+		template<typename T>
+		MemoryObject createMemoryObject(uint64_t element_count, int32_t mode = ReadOnly)
+		{
+			return MemoryObject(m_context, sizeof(T), element_count, mode);
+		}
+
+	private:
+		cl_context m_context;
+		std::vector<cl_device_id> m_devices;
+	};
+
+
 	class Wrapper
 	{
 	public:
-		enum PlatformType {
-			CPU = CL_DEVICE_TYPE_CPU,
-			GPU = CL_DEVICE_TYPE_GPU
-		};
-
 		Wrapper() = default;
 
 		void fetchPlatforms(const uint32_t num = 1u)
@@ -32,23 +213,9 @@ namespace oclw
 			return m_platforms;
 		}
 
-		cl_context createContext(cl_platform_id platform_id, PlatformType type) const
+		Context createContext(cl_platform_id platform_id, PlatformType type) const
 		{
-			cl_context context = nullptr;
-			cl_int errNum;
-			cl_context_properties contextProperties[] = {
-				CL_CONTEXT_PLATFORM,
-				(cl_context_properties) platform_id,
-				0
-			};
-			
-			context = clCreateContextFromType(contextProperties, type, NULL, NULL, &errNum);
-			if (errNum != CL_SUCCESS)
-			{
-				return nullptr;
-			}
-
-			return context;
+			return Context(platform_id, type);
 		}
 
 	private:
--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,7 @@
 #include <CL/opencl.h>
 #include <ocl_wrapper.hpp>
 
-cl_context createDefaultContext(oclw::Wrapper& wrapper)
+oclw::Context createDefaultContext(oclw::Wrapper& wrapper)
 {
 	auto platforms = wrapper.getPlatforms();
 	if (platforms.empty()) {
@@ -10,11 +10,11 @@ cl_context createDefaultContext(oclw::Wrapper& wrapper)
 	}
 	// Trying to create GPU context
 	std::cout << "Creating context on GPU..." << std::endl;
-	cl_context context = wrapper.createContext(platforms.front(), oclw::Wrapper::GPU);
+	oclw::Context context = wrapper.createContext(platforms.front(), oclw::GPU);
 	if (!context) {
 		// If not available try on CPU
 		std::cout << "Creating context on CPU..." << std::endl;
-		context = wrapper.createContext(platforms.front(), oclw::Wrapper::CPU);
+		context = wrapper.createContext(platforms.front(), oclw::CPU);
 		if (!context) {
 			std::cout << "Cannot create context." << std::endl;
 			return nullptr;
@@ -30,7 +30,42 @@ int main()
 	oclw::Wrapper wrapper;
 	wrapper.fetchPlatforms(1);
 
-	cl_context context = createDefaultContext(wrapper);
+	// Retrieve context
+	oclw::Context context = createDefaultContext(wrapper);
+	
+	// Get devices
+	auto& devices_list = context.getDevices();
+	cl_device_id device = devices_list.front();
+	
+	cl_command_queue command_queue = context.createQueue(device);
+	
+	// Create OpenCL program from HelloWorld.cl kernel source
+	oclw::Program program = context.createProgram(device, "HelloWorld.cl");
+
+	// Create OpenCL kernel
+	cl_kernel kernel = program.createKernel("hello_kernel");
+	
+	// Create memory objects that will be used as arguments to
+	// kernel. First create host memory arrays that will be
+	// used to store the arguments to the kernel
+	constexpr uint64_t ARRAY_SIZE = 128;
+	float result[ARRAY_SIZE];
+	std::vector<float> a(ARRAY_SIZE);
+	std::vector<float> b(ARRAY_SIZE);
+	for (int i = 0; i < ARRAY_SIZE; i++)
+	{
+		a[i] = (float)i;
+		b[i] = (float)(i * 2);
+	}
+
+	oclw::MemoryObject buff_a = context.createMemoryObject(a, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR);
+	oclw::MemoryObject buff_b = context.createMemoryObject(b, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR);
+	oclw::MemoryObject buff_c = context.createMemoryObject<float>(ARRAY_SIZE, CL_MEM_READ_WRITE);
+
+	if (!buff_a || !buff_b || !buff_c)
+	{
+		return -1;
+	}
 
 	return 0;
 }
